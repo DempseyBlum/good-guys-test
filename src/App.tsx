15900b7c6ac19448edb6fa259f1d32e040bae9a6
dempsey <dempseyblum@gmail.com>
2024-04-14 16:47:17 +1000

    GGT - Add fetch logic for reviews

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import ReviewsList from "./components/reviewsList/reviewsList";
+import { Review } from "./components/reviewsList/reviewsList.types";
 
 function App() {
+  const [reviews, setReviews] = React.useState<Review[]>([]);
+
+  const getReviews = () => {
+    fetch("./reviews.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data as Review[]);
+      });
+  };
+
+  useEffect(() => {
+    getReviews();
+  }, []);
+
+  useEffect(() => {
+    console.log(reviews);
+  }, [reviews]);
+
   return (
     <div className="App">
       <header className="App-header">
